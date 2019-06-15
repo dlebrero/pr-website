3ad6892ba6ef7f5e3f73cd2e7ec7f7f87560d642
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2019-06-15 11:01:15 -0300

    [#4] O nome correto da tabela deve ser EXPERIENCIAS, no plural.

--- scripts/0001 - Criação da tabela EXPERIENCIA.sql
+++ scripts/0001 - Criação da tabela EXPERIENCIA.sql
@@ -1,7 +1,4 @@
-CREATE TABLE EXPERIENCIA (# Introduction to pr-website
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
-
+CREATE TABLE EXPERIENCIAS (
   ID MEDIUMINT NOT NULL AUTO_INCREMENT,
   DATA_INICIO DATE,
   DATA_FIM DATE,
